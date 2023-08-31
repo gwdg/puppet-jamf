cee1cf9bb767c69caaa6745bc52c03e408ec515c
Tyler Sparr <tyler.sparr@encore.tech>
2023-08-31 14:18:40 -0400

    Adding Rhel8 Support

--- manifests/mysql.pp
+++ manifests/mysql.pp
@@ -1,14 +1,15 @@
 # @summary Installs and configures mysql on target host
 # @api private
 class jamf::mysql (
-  Optional[Hash]   $db            = $jamf::db,
-  Optional[Hash]   $overrides     = $jamf::mysql_overrides,
-  Optional[String] $root_pass     = $jamf::mysql_root_pass,
-  Optional[String] $version       = $jamf::mysql_version,
-  String           $os_arch       = $jamf::os_arch,
-  String           $os_version    = $jamf::os_version,
-  String           $repo_base_url = $jamf::repo_base_url,
-  String           $repo_gpgkey   = $jamf::repo_gpgkey,
+  Optional[Hash]   $db                    = $jamf::db,
+  Optional[Hash]   $overrides             = $jamf::mysql_overrides,
+  Optional[String] $root_pass             = $jamf::mysql_root_pass,
+  Optional[String] $version               = $jamf::mysql_version,
+  String           $os_arch               = $jamf::os_arch,
+  String           $os_version            = $jamf::os_version,
+  String           $repo_base_url         = $jamf::repo_base_url,
+  String           $repo_gpgkey           = $jamf::repo_gpgkey,
+  Boolean          $default_mysql_disable = $jamf::params::default_mysql_disable
 ) {
   # Set final MySQL repo URL
   $mysql_repo_url = "${repo_base_url}/mysql-${version}-community/el/${os_version}/${os_arch}/"
